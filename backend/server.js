dbe62e1d408e7632d8124fdec5f4863dd6877ec5
Iya Smith <siyana873@gmail.com>
2023-08-30 10:04:38 -0500

    inital server created, i can test postman routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'Get Goals'})
+})
+
 app.listen(port, () => {
     console.log(`Starting on port: ${port}`)
 })
